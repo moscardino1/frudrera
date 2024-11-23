9591341bfe85c95bca2b252058800f88d4437a4e
moscardino1 <alessandrocarli90@gmail.com>
2024-11-23 10:55:15 -0500

    look updates

--- app.py
+++ app.py
@@ -7,9 +7,23 @@ load_dotenv()
 from config import Config
 from detection import detect_objects
 from recipe import recommend_recipe
+from io import BytesIO
+import qrcode
 
 app = Flask(__name__)
 app.config.from_object(Config)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/info')
+def info():
+    return render_template('info.html')
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -40,6 +54,34 @@ def index():
 @app.route('/results')
 def results():
     return render_template('results.html')
+
+
+@app.route('/donate')
+def donate():
+    USDT_ADDRESS = "0xDC92534Be92780c87f232CD525D99e26892E15f7"
+    
+    # Generate QR code
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+    qr.add_data(USDT_ADDRESS)
+    qr.make(fit=True)
+
+    # Create an image from the QR Code instance
+    img = qr.make_image(fill_color="black", back_color="white")
+
+    # Save the image to a BytesIO object
+    buffered = BytesIO()
+    img.save(buffered, format="PNG")
+
+    # Encode the image to Base64
+    qr_image = base64.b64encode(buffered.getvalue()).decode('utf-8')
+    
+    return render_template('donate.html', usdt_address=USDT_ADDRESS, qr_image=qr_image)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
